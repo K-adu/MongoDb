cbb390aad829befca1dde57def0a558e3f6a1468
K-adu <manishbaral112@gmail.com>
2023-05-30 11:17:30 +0545

    build:final

--- TaskApp/src/index.js
+++ TaskApp/src/index.js
@@ -1,12 +1,15 @@
+
+const port = process.env.PORT
 const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 3000
 
-
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
 
 
 app.listen(port, () => {
@@ -14,37 +17,6 @@ app.listen(port, () => {
 })
 
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
-
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-// testing multer
-
-
-// we need to configure multer
-//making a new multer instance
-
-
-  
 
 
 
-
-
-
-
-const main = async () => {
-    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    // const user = await User.findById('5c2e4dcb5eac678a23725b5b')
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks)
-}
-
-main()
